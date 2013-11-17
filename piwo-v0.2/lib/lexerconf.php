e94c67cd30b9e51e4e5130df779fff277274fb92
pitiz29a@gmail.com <pitiz29a@gmail.com@fb5733c1-9931-fd08-9e05-17011bb96e3f>
2013-11-17 12:40:02 +0000

    TestingTools output

--- piwo-v0.2/lib/lexerconf.php
+++ piwo-v0.2/lib/lexerconf.php
@@ -7,31 +7,17 @@ require_once INC_PATH.'piwo-v0.2/cfg/main.php';require_once INC_PATH.'piwo-v0.2
 foreach ($parserTokenList as $parserToken) {
 	require_once $parserToken;
 }function parse($txt) {		$debugCatchedException = false;
-	$debugString = "";
-		try {		$loglevel = Log::INFO;		if(pw_wiki_getcfg('debug') === true) {			$loglevel = Log::DEBUG;		}		$lexer = new Lexer($txt, $loglevel);				$handlerList = array(				new Header(),				new Border(),				new BorderError(),				new BorderInfo(),				new BorderSuccess(),				new BorderValidation(),				new BorderWarning(),				new Plugin(),				new PluginParameter(),
+		try {		$lexer = new Lexer($txt);		$handlerList = array(				new Header(),				new Border(),				new BorderError(),				new BorderInfo(),				new BorderSuccess(),				new BorderValidation(),				new BorderWarning(),				new Plugin(),				new PluginParameter(),
 				new InternalLink(),				new InternalLinkText(),				new InternalLinkMode(),				new InternalLinkPos(),				new Url(),				new UrlNoProtocol(),				new Big(),				new Bold(),				new Em(),				new Hi(),				new Italic(),				new Lo(), 				new Monospace(),				new Small(),				new Strike(),				new Sub(),				new Sup(),				new Underline(),				new Code(),				new NoWiki(),				new NoWikiAlt(),				new Newline(),				new Multiline(),				new Preformat(),				new Align(),				new Justify(),				new Indent(),				new Right(),				new Left(),				new Constant(),				new Symbol(),				new Variable(),				new ExternalLink(),				new ExternalLinkPos(),				new Pre(),				new TableCell(),				new TableRow(),				new TableHeader(),				new Table(),				new TableSpan(),				new AlignInTable(),				new HorizontalRule(),				new DefTerm(),				new DefList(),				new DefItem(),				new ListItem(),				new Lists(),				new Footnote(),				new QuotedString(),				new Math(),				new NoToc(),				new Comment(),				new Comment2()		);				$lexer->registerHandlerList($handlerList);				//TODO No pattern? AST = #DOCUMENT with a single #TEXT node
 		$lexer->parse();		$parser = new Parser();		$parser->registerHandlerList($handlerList);
-		$parser->setUserInfo('indextable', WikiTocTools::createIndexTable($parser, $lexer->getRootNode()));		$parser->setUserInfo('lexerperformance', $lexer->getExecutionTime());		$parser->setUserInfo('piwoversion', PIWOVERSION);				$_SESSION["pw_wiki"]["error"] = false;		 		$ta = new TreeWalker($lexer->getRootNode(), $parser);
+		$parser->setUserInfo('indextable', WikiTocTools::createIndexTable($parser, $lexer->getRootNode()));		$parser->setUserInfo('lexerperformance', $lexer->getExecutionTime());		$parser->setUserInfo('piwoversion', PIWOVERSION);		 		$ta = new TreeWalker($lexer->getRootNode(), $parser);
  		$o = implode($ta->getResult());
-	} catch (Exception $e) {		$debugCatchedException = true;		$src = "N/A";		$log = "N/A";		if (isset($lexer)) {			$err = $lexer->getLog()->getLastLog();			$log = $lexer->getLog();			$src = $lexer->getSource();		}		$debugString .= "<h3>Exception catched! Logfile output...</h3>";		$debugString .= "<pre style='white-space: pre-wrap'>";		$debugString .= "ERROR MESSAGE: \n".pw_s2e(print_r($e->getMessage(), true));		$debugString .= "\n\nERROR TRACE: \n".pw_s2e($e->getTraceAsString());		$debugString .= "\n\nSOURCE: \n".pw_s2e(StringTools::showLineNumbers($src));		$debugString .= "\n\nLOG: \n".$log;		$debugString .= "</pre>";	}		if ($debugCatchedException || pw_wiki_getcfg('debug')) {			if (isset($lexer)) {
-			$debugString .= "PATTERNTABLE: \n";
-			$debugString .= $lexer->getPatternTableAsString();
-		}
-				$debugString .= "LEXER: \n";
-		$debugString .= $lexer;
-	
-		$debugString .= "AST: \n";
-		$treePrinter = new TreeWalker($lexer->getRootNode(), new TreePrinter());
-		$ast = $treePrinter->getResult();
-		$debugString .= StringTools::showLineNumbers($ast);
-	
-		$debugString .= "Performance:\n";
-		$debugString .= "Text parsed in ".$lexer->getExecutionTime()."s!";
+	} catch (Exception $e) {		$debugCatchedException = true;		TestingTools::inform("Exception catched! ERROR MESSAGE: ".pw_s2e(print_r($e->getMessage(), true)));		TestingTools::inform("ERROR TRACE: \n".pw_s2e($e->getTraceAsString()));	}		if ($debugCatchedException || pw_wiki_getcfg('debug')) {			TestingTools::inform("LEXER: ".$lexer, TestingTools::NOTYPEINFO);				if (isset($lexer)) {
+			TestingTools::inform("PATTERN TABLE: \n" . $lexer->getPatternTableAsString());
+			$treePrinter = new TreeWalker($lexer->getRootNode(), new TreePrinter());			TestingTools::inform("PARSE TREE: \n".StringTools::showLineNumbers($treePrinter->getResult()));			TestingTools::inform("SPEED: Text parsed in ".$lexer->getExecutionTime()." seconds!");			TestingTools::inform("SOURCE:\n".StringTools::showLineNumbers($lexer->getSource()));		}
 	
 		//$debugString .= "<h3>Debug: Parser - Schritte (TODO: ADAPT TO NEW LEXER)</h3>";
 		//$lexer->printDebugInfo(1,1);
-	
-		$debugString .= "SOURCE:\n";		$debugString .= StringTools::preFormatShowLineNumbers($lexer->getSource());
 	
 	}	return $o;
 }function pw_wiki_lexerconf(Lexer $lexer) {
