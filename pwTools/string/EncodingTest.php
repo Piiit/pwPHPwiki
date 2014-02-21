bd6a28d0fa3f07f7f32ce0a08c96ed399a4a33d8
pitiz29a@gmail.com <pitiz29a@gmail.com@fb5733c1-9931-fd08-9e05-17011bb96e3f>
2014-02-21 11:04:45 +0000

    Encoding issues: pw_s2e... utf8 or ascii to html entities

--- pwTools/string/EncodingTest.php
+++ pwTools/string/EncodingTest.php
@@ -2,12 +2,24 @@
 if (!defined('INC_PATH')) {
 	define ('INC_PATH', realpath(dirname(__FILE__).'/../../').'/');
 }
-require_once INC_PATH.'pwTools/debug/TestingTools.php';
 require_once INC_PATH.'pwTools/string/encoding.php';
+require_once 'PHPUnit/Framework/TestCase.php';
 
-TestingTools::outputOn();
-TestingTools::logOn();
-TestingTools::debugOn();
+class EncodingTest extends PHPUnit_Framework_TestCase {
+	
+	protected function setUp() {
+		parent::setUp();
+	}
+	
+	protected function tearDown() {
+		parent::tearDown();
+	}
+	
+	public function testPw_s2e() {
+		$this->assertEquals("&auml;", pw_s2e("ä"), "Result: ".pw_s2e("ä"));		
+	}
+}
+	
 
-echo pw_s2e("ä"); 
+ 
 
