320c7624872dc3defb2b95dc414b6600519e4911
Piiit <pitiz29a@gmail.com>
2015-06-16 12:10:32 +0200

    admin.php removed

--- lib/admin.php
+++
@@ -1,9 +0,0 @@
-<?phpif (!defined('INC_PATH')) {
-	define ('INC_PATH', realpath(dirname(__FILE__).'/../../').'/');
-}
-require_once INC_PATH.'piwo-v0.2/lib/common.php';require_once INC_PATH.'pwTools/string/encoding.php';require_once INC_PATH.'pwTools/debug/TestingTools.php';require_once INC_PATH.'pwTools/file/FileTools.php';
-require_once INC_PATH.'pwTools/gui/GuiTools.php';
-
-
-
-?>
