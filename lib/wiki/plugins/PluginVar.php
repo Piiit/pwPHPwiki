4f5938e099f330a85e84130e5422ffc0bb7368bd
Piiit <pitiz29a@gmail.com>
2015-06-17 13:50:49 +0200

    WIP: Variable plugin.

--- lib/wiki/plugins/PluginVar.php
+++ lib/wiki/plugins/PluginVar.php
@@ -1,6 +1,13 @@
 <?php
 
+/*
+ * TODO: varvalues > array syntax for values
+ * TODO: varnames  > error handling for illegal variable names (define clear regexp for varnames
+ * TODO: varvalues > multiline value support
+ */
 class PluginVar implements WikiPluginHandler {
+	
+	private static $variables = array();
 	
 	public function getPluginName() {
 		return "var";
@@ -18,43 +25,52 @@ class PluginVar implements WikiPluginHandler {
 			return nop("Plugin '".$this->getPluginName()."': No variable name specified.");
 		}
 		
-		$out = null;
 		foreach ($node->getChildren() as $parameterNode) {
 			if(strcasecmp("pluginparameter", $parameterNode->getName()) == 0) {
 				$token = new ParserRule($parameterNode, $parser);
 				$text = $token->getText();
 				
-				//TODO do this with quoted string tokens...
-				//TODO add multiline values support
 				$matches = array();
+				
+				/*
+				 * Creating or updating variables.
+				 * FIXME This is a bad regexp... what is allowed exactly for varnames and values
+				 */
 				if (preg_match("#([\w]+) *= *(.*)#i", $text, $matches)) {
 					$varname = utf8_strtolower(utf8_trim($matches[1]));
 					$value = utf8_trim($matches[2]);
-					Variable::$variables[$varname] = $value;
+					self::$variables[$varname] = $value;
 					TestingTools::debug("Variable: adding '$varname' with value '$value'");
-				} else {
-					$varname = utf8_strtolower($text);
-					if (isset(Variable::$variables[$varname])) {
-						$out = Variable::$variables[$varname];
-						$out = self::unescape($out);
-					} elseif (preg_match("#(.*) *= *(.*)#i", $text, $matches)) {
-						$varname = utf8_trim($matches[1]);
-						return nop("VARIABLE '$varname' contains illegal characters.");
-					} else {
-						$_SESSION['pw_wiki']['error'] = true;
-						return nop("VARIABLE '$varname' undefined.");
-					}
+					
+					/*
+					 * Nothing to output, just store variables and values...
+					 */
+					return;
+				}
+
+				/*
+				 * Returning existing variable values.
+				 */
+				$varname = utf8_strtolower($text);
+				if (isset(self::$variables[$varname])) {
+    				return self::$variables[$varname];
+				} 
+				
+				/*
+				 * Nothing found, check for illegal variable names.
+				 * FIXME This should be done at the beginning, according to a unique clear regexp.
+				 */
+				if (preg_match("#(.*) *= *(.*)#i", $text, $matches)) {
+					$varname = utf8_trim($matches[1]);
+					return nop("VARIABLE '$varname' contains illegal characters.");
 				}
 			}
 		}
 		
-	    $out = pw_s2e($out);
-	    return $out;
-
-	}
-	
-	private static function unescape($txt) {
-		return str_replace(array('\"', '\>'), array('"', '&gt;'), $txt);
+		/*
+		 * Legal variable name, but not found in variable collection.
+		 */
+		return nop("VARIABLE '$varname' is undefined.");
 	}
 
 }
